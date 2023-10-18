00b2c1461382a9e835d9fe4e6454fbee242e0d6b
Michal Pasierbski <mpasierbski@gmail.com>
2023-10-18 19:19:16 +0200

    Setup remix + express

---
+++ server.mjs
@@ -0,0 +1,18 @@
+import express from "express";
+import { createRequestHandler } from "@remix-run/express";
+import * as build from "./build/index.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.all("*", createRequestHandler({ build }));
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
