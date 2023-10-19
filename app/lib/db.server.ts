c7ebc59cd65abdf48b3c5a3257c4da44248e9c7f
Michal Pasierbski <mpasierbski@gmail.com>
2023-10-19 11:29:27 +0200

    Add database boilerplate

---
+++ app/lib/db.server.ts
@@ -0,0 +1,1 @@
+export { knex } from "../../lib/knex";
---
+++ migrations/20231019075152_create_users_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable("users", function (table) {
+    table.increments("id").primary();
+    table.string("email").notNullable().unique();
+    table.timestamps(true, true);
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.dropTableIfExists("users");
+}
