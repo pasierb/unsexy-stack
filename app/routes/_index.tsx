a0ed10b852361ec8a96ef4ab2d6ebdbaf659fca3
Michal Pasierbski <mpasierbski@gmail.com>
2023-10-28 11:52:10 +0200

    Use README as homepage content

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,9 @@
+import ReadmeDoc from "../../README.md";
+
 export default function HomePage() {
   return (
-    <main className="container">
-      <h1>Home Page</h1>
+    <main className="container prose">
+      <ReadmeDoc />
     </main>
   );
 }
